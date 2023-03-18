d053733525fd36762e119855221bee47766139a3
Shmalvin <mavonialvin@gmail.com>
2023-03-17 21:22:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints exactly using the print f option
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+	return (0);
+}
