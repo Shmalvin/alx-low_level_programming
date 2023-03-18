6e9386dbb5968aae562737f3d17609d23b520fc6
Shmalvin <mavonialvin@gmail.com>
2023-03-17 21:42:54 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints the size of various size of computers hard disk
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of char: %lu byte(s)\n," (unsigned long)sizeofof(a));
+printf("Size of int: %lu byte(s)\n," (unsigned long)sizeofof(b));
+printf("Size of long int: %lu byte(s)\n," (unsigned long)sizeofof(c));
+printf("Size of long long int: %lu byte(s)\n," (unsigned long)sizeofof(d));
+printf("size of float: %lu byte(s)\n," (unsigned long)sizeofof(f));
+return (0);
+}
